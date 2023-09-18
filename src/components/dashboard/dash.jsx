97acb65bbe768338baa7db9af4601dcb17d105ac
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-18 19:44:03 +0300

    vendor

--- src/components/dashboard/dash.jsx
+++ src/components/dashboard/dash.jsx
@@ -1,70 +1,32 @@
-import React from 'react'
-import './dash.css'
+import React from "react";
+import "./dash.css";
 
+function Dashboard() {
+  return (
+    <>
+      <section className="dashboard">
+        <div className="dash">
+          <div className="products">
+            <div className="product-cont"></div>
 
+            <div className="product-detail"></div>
+          </div>
 
+          <div className="products">
+            <div className="product-cont"></div>
 
-function Dashboard (){
+            <div className="product-detail"></div>
+          </div>
 
-    return(
+          <div className="products">
+            <div className="product-cont"></div>
 
-        <>
-
-            <section className='dashboard'>
-
-                <div className='dash'>
-
-                    <div className='products'>
-
-                        <div className='product-cont'>
-
-                        </div>
-
-                        <div className='product-detail'>
-
-                        </div>
-
-
-                    </div>
-
-                    <div className='products'>
-
-                    <div className='product-cont'>
-
-</div>
-
-<div className='product-detail'>
-
-</div>
-
-                        
-                    </div>
-
-                    <div className='products'>
-
-                    <div className='product-cont'>
-
-</div>
-
-<div className='product-detail'>
-
-</div>
-
-                        
-                    </div>
-
-                </div>
-
-
-
-            </section>
-
-
-
-
-        </>
-    )
+            <div className="product-detail"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-
-export default Dashboard
+export default Dashboard;
