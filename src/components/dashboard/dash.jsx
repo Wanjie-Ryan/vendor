30cf65c92069e4f3b6aadbfad11c35f77c075cda
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-18 19:43:29 +0300

    vendor

--- src/components/dashboard/dash.jsx
+++ src/components/dashboard/dash.jsx
@@ -10,6 +10,57 @@ function Dashboard (){
 
         <>
 
+            <section className='dashboard'>
+
+                <div className='dash'>
+
+                    <div className='products'>
+
+                        <div className='product-cont'>
+
+                        </div>
+
+                        <div className='product-detail'>
+
+                        </div>
+
+
+                    </div>
+
+                    <div className='products'>
+
+                    <div className='product-cont'>
+
+</div>
+
+<div className='product-detail'>
+
+</div>
+
+                        
+                    </div>
+
+                    <div className='products'>
+
+                    <div className='product-cont'>
+
+</div>
+
+<div className='product-detail'>
+
+</div>
+
+                        
+                    </div>
+
+                </div>
+
+
+
+            </section>
+
+
+
 
         </>
     )
