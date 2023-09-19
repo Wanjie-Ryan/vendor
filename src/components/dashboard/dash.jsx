090db1e55b6b382bb94b1b81e360a345390a7a2c
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-19 06:01:33 +0300

    vendor

--- src/components/dashboard/dash.jsx
+++ src/components/dashboard/dash.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./dash.css";
-import {MdProductionQuantityLimits} from 'react-icons/md'
-import {FaWallet} from 'react-icons/fa'
-import {FcSalesPerformance} from 'react-icons/fc'
-import {Link} from 'react-router-dom'
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { FaWallet } from "react-icons/fa";
+import { FcSalesPerformance } from "react-icons/fc";
+import { Link } from "react-router-dom";
 function Dashboard() {
   return (
     <>
@@ -12,10 +12,12 @@ function Dashboard() {
           <div className="products">
             <div className="product-cont">
               <div className="icon-product">
-                <MdProductionQuantityLimits className="icon-products"/>
+                <MdProductionQuantityLimits className="icon-products" />
               </div>
               <div className="product-name">
-                <Link to='/products' className="products-link"><h3>Check Products</h3></Link>
+                <Link to="/products" className="products-link">
+                  <h3>Check Products</h3>
+                </Link>
               </div>
             </div>
 
@@ -25,10 +27,12 @@ function Dashboard() {
           <div className="products">
             <div className="product-cont">
               <div className="icon-product">
-                <FaWallet className="icon-products"/>
+                <FaWallet className="icon-products" />
               </div>
               <div className="product-name">
-              <Link to='/wallet' className="products-link"><h3>Your Wallet</h3></Link>
+                <Link to="/wallet" className="products-link">
+                  <h3>Your Wallet</h3>
+                </Link>
               </div>
             </div>
 
@@ -38,10 +42,12 @@ function Dashboard() {
           <div className="products">
             <div className="product-cont">
               <div className="icon-product">
-                <FcSalesPerformance className="icon-products"/>
+                <FcSalesPerformance className="icon-products" />
               </div>
               <div className="product-name">
-              <Link to='/sales' className="products-link"><h3>Your Sales</h3></Link>
+                <Link to="/sales" className="products-link">
+                  <h3>Your Sales</h3>
+                </Link>
               </div>
             </div>
 
