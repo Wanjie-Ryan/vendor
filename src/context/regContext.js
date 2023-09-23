76868f030153b4c6df11091ebc83fd2e2d8cbb10
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-23 06:52:04 +0300

    vendor integration-part 1

--- src/context/regContext.js
+++ src/context/regContext.js
@@ -1,5 +1,58 @@
-import React, {createContext, useContext, useReducer} from 'react'
+import React, {createContext, useReducer} from 'react'
 
-const RegisterData ={
-    
+const initialState ={
+
+    vendor:null,
+    loading:false,
+    error:null
+
+}
+
+export const RegContext = createContext(initialState)
+
+const regReducer = (action,state)=>{
+    switch(action.type){
+        case 'regStart':
+            return{
+
+                vendor:null,
+                loading:true,
+                error:null
+            }
+
+        case 'regComplete':
+            return{
+                vendor:action.payload,
+                loading:false,
+                error:null
+            }
+        
+        case 'regFail':
+            return{
+                vendor:null,
+                loading:false,
+                error:action.payload
+            }
+
+        default:
+            return state
+    }
+}
+
+export const RegContextProvider = ({children})=>{
+
+    const [state, dispatch] = useReducer(regReducer, initialState)
+
+    return(
+        <RegContext.Provider value={{
+            vendor:state.vendor,
+            loading:state.loading,
+            error:state.error,
+            dispatch
+        }}>
+            {children}
+        </RegContext.Provider>
+    )
+
+
 }
