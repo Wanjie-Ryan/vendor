997958913667ae0060d5eda28a59c60beb95b562
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-23 11:34:53 +0300

    vendor integration-part 1

--- src/components/dashboard/dash.jsx
+++ src/components/dashboard/dash.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./dash.css";
 import { MdProductionQuantityLimits } from "react-icons/md";
 import { FaWallet } from "react-icons/fa";
 import { FcSalesPerformance } from "react-icons/fc";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
 function Dashboard() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      if (!Cookies.get().vendorToken || Cookies.get().vendorToken === undefined) {
+        navigate("/login");
+      } else {
+        const token = Cookies.get().vendorToken;
+
+        const res = await axios({
+          method: "get",
+          url: "http://localhost:3005/api/vendor/auth/verify",
+          headers: { Authorization: "Bearer " + token },
+          data: {},
+        });
+
+        if (res.data.type !== "success") {
+          navigate("/login");
+        }
+      }
+    };
+
+    checkAuth();
+  }, [navigate]);
+
+
   return (
     <>
       <section className="dashboard">
