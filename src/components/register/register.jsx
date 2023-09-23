2069882ac89b7f5c3d1a756494b57f28689973d9
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-23 07:38:02 +0300

    vendor integration-part 1

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -21,7 +21,7 @@ function Register() {
   const [load, setLoad] = useState(false);
   const [errormsg, setErrormsg] = useState();
 
-  const { user, loading, error, dispatch } = useContext(RegContext);
+  const { vendor, loading, error, dispatch } = useContext(RegContext);
 
   const TogglePwd = () => {
     setCheckpwd(!checkpwd);
@@ -44,6 +44,86 @@ function Register() {
     setPwd(e.target.value);
   };
 
+  const handleRegistration = async(e)=>{
+    e.preventDefault();
+
+    if (!name || !email || !contact  || !pwd) {
+      toast.error("Please fill in all the fields");
+      return;
+    }
+
+    setLoad(true);
+
+    dispatch({ type: "regStart" });
+
+    try{
+
+      const registrationData = {
+
+        name: name,
+        email: email,
+        contact: contact,
+        password: pwd,
+      };
+
+      const response = await axios.post(
+        "http://localhost:3005/api/vendor/auth/register",
+        registrationData
+      );
+
+      // console.log(response)
+
+      dispatch({ type: "regComplete", payload: response.data });
+
+      toast.success("Registration Successful");
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 1000);
+
+      setLoad(false);
+
+
+    }
+
+    catch(err){
+
+      dispatch({ type: "regFail", payload: err });
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (pwd.length < 5) {
+        toast.error(
+          "Password is too short. Please enter at least 5 characters."
+        );
+        setLoad(false);
+
+        return;
+      } else if (!emailRegex.test(email)) {
+        toast.error("Invalid email address. Please enter a valid email.");
+        setLoad(false);
+
+        return;
+      } else {
+        setErrormsg(
+          "There seems to be an error, refresh the page and try again!"
+        );
+
+        toast.error(errormsg);
+        setLoad(false);
+      }
+
+
+
+    }
+    finally{
+
+      setLoad(false)
+
+    }
+
+
+  }
+
   return (
     <>
       <section className="register">
@@ -51,7 +131,7 @@ function Register() {
           <p className="home-link title">Superb Kitchen-Vendor</p>
           <p className="register-text">Register</p>
 
-          <form className="form">
+          <form className="form" onSubmit={handleRegistration}>
             <div className="form-container">
               <div className="name-label">
                 <label>Name</label>
@@ -77,6 +157,7 @@ function Register() {
                 <PhoneInput
                   placeholder="Your Phone Number"
                   className="pwd-input"
+                  required
                   onChange={handleContact}
                   value={contact}
                 />
@@ -88,13 +169,18 @@ function Register() {
                 <label>Password</label>
               </div>
               <div className="input-container-pwd">
-                <input type="password" placeholder="enter your Password" />
-                <AiOutlineEye className="toggle-password" />
+                <input type={checkpwd ? "text" : "password"} placeholder="enter your Password" required value={pwd}
+                  onChange={handlePwd} />
+                <AiOutlineEye className="toggle-password" onClick={TogglePwd}/>
               </div>
             </div>
 
             <button type="submit" className="submit-btn">
-              Let's Go
+            {load ? (
+                <AiOutlineLoading3Quarters className="loading-icon" />
+              ) : (
+                "Let's Go"
+              )}
             </button>
             <Link to="/login" className="login-p">
               <p>Already Registered? Sign-In</p>
