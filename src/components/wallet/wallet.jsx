0b5fa15b8a160607b9e7b010a6d52437f102e596
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-29 14:07:46 +0300

    wallet

--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -1,9 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./wallet.css";
 import { FaFileCsv } from "react-icons/fa";
 import { BsFillFileEarmarkPdfFill } from "react-icons/bs";
 import axios from 'axios'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Cookies from "js-cookie";
+import {useNavigate} from 'react-router-dom'
 function Wallet() {
+
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
+  const [products, setProducts] = useState([])
+  console.log(products)
+
+  useEffect(()=>{
+
+    const getPurchasedProducts = async()=>{
+
+      try{
+
+        const token = Cookies.get().vendorToken;
+
+        const response = await axios.get('http://localhost:3005/api/vendor/products/purchasedproducts', {headers:{Authorization:`Bearer ${token}`}})
+
+        // console.log(response.data.purchasedProducts)
+
+        setProducts(response.data.purchasedProducts)
+      }
+
+      catch(err){
+
+        // console.log(err)
+        if (err.response.status === 401) {
+          toast.error("Not Authorized");
+          navigate("/login");
+        } else if (err.response.status === 500) {
+          toast.error("A problem with our servers, hang on");
+        }
+      }
+
+
+
+
+    }
+
+    getPurchasedProducts()
+
+
+  },[])
   return (
     <>
       <section className="records">
@@ -26,22 +71,30 @@ function Wallet() {
                 <th>No.</th>
                 <th>Product Image</th>
                 <th>Product</th>
-                {/* <th>Quantity</th> */}
+                <th>Bought By</th>
                 <th>Amount</th>
                 <th>Payment Status</th>
-                <th>Delivery Status</th>
+                <th>Receive Payment</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td><img src='' alt='image' className='product-image'/></td>
-                <td>xxx</td>
-                {/* <td>3</td> */}
-                <td>Ksh. 10</td>
-                <td className="paid">xxx</td>
-                <td className="pending">yyyy</td>
+              {products.length ===0?(
+                <p>Your products have not yet been purchased</p>
+              ):products.map((item, index)=>(
+
+                <tr key={item._id}>
+                <td>{index+1}</td>
+                <td><img src={item.image} alt='image' className='product-image'/></td>
+                <td>{item.name}</td>
+                <td>yyy</td>
+                <td>{`ksh. ${item.price}`}</td>
+                <td className="paid">paid</td>
+                <td>
+                  <button className='receive-payment'>Receive Payment</button>
+                </td>
               </tr>
+                ))
+              }
             </tbody>
           </table>
           
