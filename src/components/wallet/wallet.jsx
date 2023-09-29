4538f2439b027b74f6fa326e14ffbcbd89ba312f
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-29 13:39:42 +0300

    wallet

--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -1,71 +1,54 @@
-import React, {useState} from 'react'
-import './wallet.css'
-import {FaFileCsv} from 'react-icons/fa'
-import {BsFillFileEarmarkPdfFill} from 'react-icons/bs'
+import React, { useState } from "react";
+import "./wallet.css";
+import { FaFileCsv } from "react-icons/fa";
+import { BsFillFileEarmarkPdfFill } from "react-icons/bs";
 
-function Wallet(){
+function Wallet() {
+  return (
+    <>
+      <section className="records">
+        <div className="table-container">
 
+          <div className="table-btns">
+            <button className="csv">
+              <FaFileCsv /> Export as CSV
+            </button>
+            <button className="pdf">
+              <BsFillFileEarmarkPdfFill />
+              Export as PDF
+            </button>
+          </div>
+          
 
-    return(
-
-        <>
-
-<div className="table-container">
-                <div className="table-btns">
-                  <button className="csv">
-                    <FaFileCsv /> Export as CSV
-                  </button>
-                  <button className="pdf">
-                    <BsFillFileEarmarkPdfFill />
-                    Export as PDF
-                  </button>
-                </div>
-                <table className="products-table">
-                  <thead>
-                    <tr>
-                      <th>No</th>
-                      <th>Image</th>
-                      <th>Name</th>
-                      <th>Bought By</th>
-                      <th>Amount</th>
-                      <th>Receive Payment</th>
-                      
-                    </tr>
-                  </thead>
-                  <tbody>
-                    
-                        <tr >
-                          <td>1</td>
-                          <td>
-                            <img
-                              src=''
-                              alt="image"
-                              className="product-image"
-                            />
-                          </td>
-                          <td>xxxxx</td>
-                          <td>ksh 20</td>
-                          <td>40</td>
-                          <td>
-                            <button
-                              className="edit"
-                              
-                            >
-                              Receive Payment
-                            </button>
-                          </td>
-                          
-                        </tr>
-                     
-                  </tbody>
-                </table>
-              </div>
-        
-        
-        
-        </>
-    )
+          <table className="products-table--">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Product Image</th>
+                <th>Product</th>
+                {/* <th>Quantity</th> */}
+                <th>Amount</th>
+                <th>Payment Status</th>
+                <th>Delivery Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>image</td>
+                <td>xxx</td>
+                {/* <td>3</td> */}
+                <td>Ksh. 10</td>
+                <td className="paid">xxx</td>
+                <td className="pending">yyyy</td>
+              </tr>
+            </tbody>
+          </table>
+          
+        </div>
+      </section>
+    </>
+  );
 }
 
-
-export default Wallet
+export default Wallet;
